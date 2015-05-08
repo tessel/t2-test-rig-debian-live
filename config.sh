c643442e0d5179e89021a7976ff3093f45129555
Kevin Mehall <km@kevinmehall.net>
2015-05-08 17:35:04 +0000

    Initial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+lb config \
+	--architecture i386 \
+	--linux-flavours 686-pae \
+	--distribution jessie \
+	--parent-distribution jessie \
+	--binary-images hdd \
+	--parent-mirror-bootstrap http://ftp.us.debian.org/debian/
